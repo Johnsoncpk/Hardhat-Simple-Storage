a3c7cc868534b59288268bb925d89f9aca54a27b
Johnsonc <chong12319@gmail.com>
2022-06-13 16:40:57 +0800

    simple storage

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,14 +1,12 @@
+const { clearConfigCache } = require("prettier");
+require("@nomiclabs/hardhat-etherscan");
 require("@nomiclabs/hardhat-waffle");
 require("dotenv").config();
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+require("./tasks/accounts");
+require("./tasks/block-number");
+require("hardhat-gas-reporter");
+require("solidity-coverage");
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
@@ -19,15 +17,31 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 
 const RINKBY_RPC_URL = process.env.RINKEBY_RPC_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
-
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY;
 module.exports = {
-  // defaultNetwork: "hardhat"
+  defaultNetwork: "hardhat",
   networks: {
     rinkeby: {
       url: RINKBY_RPC_URL,
-      account: [PRIVATE_KEY],
+      accounts: [PRIVATE_KEY],
       chainId: 4,
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545/",
+      chainId: 31337,
     },
   },
   solidity: "0.8.14",
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+  gasReporter: {
+    enabled: true,
+    outputFile: "gas-report.txt",
+    noColors: true,
+    currency: "USD",
+    coinmarketcap: COINMARKETCAP_API_KEY,
+    token: "MATIC"
+  },
 };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,19 +1,51 @@
-const {ethers} = require("hardhat");
+const {ethers, run, network} = require("hardhat");
 
 async function main(){
-  const SimpleStorageFactory = await ethers.getContractFactory(
-    "SimpleStorage"
-  );
+	const SimpleStorageFactory = await ethers.getContractFactory(
+	"SimpleStorage"
+	);
 
-  console.log("Deploying contract...");
-  const simpleStorage = await SimpleStorageFactory.deploy();
-  await simpleStorage.deployed();
-    console.log(`Deployed contracct to: ${simpleStorage.address}`);
+	console.log("Deploying contract...");
+	const simpleStorage = await SimpleStorageFactory.deploy();
+	await simpleStorage.deployed();
+	console.log(`Deployed contracct to: ${simpleStorage.address}`);
+
+	if(network.config.chainId===4 && process.env.ETHERSCAN_API_KEY){
+	    console.log("Waiting for block confirmation.");
+		await simpleStorage.deployTransaction.wait(2);
+		await verify(simpleStorage.address, []);
+	}
+
+	const currentValue = await simpleStorage.retrieve();
+	console.log(`Current Value is:  ${currentValue}`);
+	
+	//Update current value
+
+	const transactionResponse = await simpleStorage.store("7");
+	await transactionResponse.wait(1);
+	const upadateValue = await simpleStorage.retrieve();
+	console.log(`Update Value is ${upadateValue}`);
+}
+
+async function verify(contractAddress, args){
+	console.log("Verifying contract...");
+	try{
+		await run("verify:verify", {
+			address: contractAddress,
+			constructorArguments: args,
+		});
+	}catch(e){
+		if(e.message.toLowerCase().includes("already verified")){
+			console.log("Already Verified!")
+		}else{
+			console.log(e);
+		}
+	}
 }
 
 main()
-  .then(()=>process.exit(0))
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+	.then(()=>process.exit(0))
+	.catch((error) => {
+	console.error(error);
+	process.exit(1);
+	});
